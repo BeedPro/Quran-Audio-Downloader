783375bdad44fb867749f36ed28d49962f09dd4c
BeedPro <annoor28@hotmail.co.uk>
2024-03-03 22:42:20 +0000

    just an update

---
+++ downloader/constants.py
@@ -0,0 +1,46 @@
+import requests
+import os
+import json
+from bs4 import BeautifulSoup
+from exceptions import FailedToFetchURLException
+
+
+class Constants:
+    def __init__(self):
+        self.QURANIC_AUDIO_LINK = "https://www.quranicaudio.com"
+        self.QURANIC_DATA_FILE_PATH = "quranic_audio_data.json"
+        self.QURANIC_AUDIO_DATA = self.__get_data_from_file()
+
+    def __get_links(self):
+        response = requests.get(self.QURANIC_AUDIO_LINK)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, "html.parser")
+            links = soup.find_all("a")
+            hrefs = [link.get("href") for link in links]
+            cleaned_hrefs = [
+                href for href in hrefs if href and not href.startswith("#")
+            ]
+            return cleaned_hrefs
+        else:
+            raise FailedToFetchURLException(
+                f"Failed to fetch URL: {self.QURANIC_AUDIO_LINK}"
+            )
+
+    def __get_data_from_file(self):
+        if not os.path.isfile(self.QURANIC_DATA_FILE_PATH):
+            data = self.__get_data()
+            with open(self.QURANIC_DATA_FILE_PATH, "w+") as json_f:
+                json.dump(data, json_f)
+        with open(self.QURANIC_DATA_FILE_PATH, "r") as json_f:
+            data = json.load(json_f)
+            return data
+
+    def __get_data(self):
+        recieter_links = self.__get_links()
+        response = requests.get(f"{self.QURANIC_AUDIO_LINK}/{recieter_links[0]}")
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, "html.parser")
+            scripts = soup.find("script")
+            if scripts:
+                return scripts.text.split("window.__data=")[1][:-1]
+        return None
--- downloader/program.py
+++ downloader/program.py
@@ -1,30 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
-from exceptions import FailedToFetchURLException
+from constants import Constants
 
-QURANIC_AUDIO_LINK = "https://www.quranicaudio.com"
-
-
-def get_links(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        links = soup.find_all("a")
-        hrefs = [link.get("href") for link in links]
-        cleaned_hrefs = [href for href in hrefs if href and not href.startswith("#")]
-        return cleaned_hrefs
-    else:
-        raise FailedToFetchURLException(f"Failed to fetch URL: {url}")
+constants = Constants()
 
 
 def main():
-    recieter_links = get_links(QURANIC_AUDIO_LINK)
-    response = requests.get(f"{QURANIC_AUDIO_LINK}/{recieter_links[0]}")
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        scripts = soup.find("script")
-        if scripts:
-            print(scripts.text.split("window.__data=")[1][:-1])
+    print(constants.QURANIC_AUDIO_DATA)
 
 
 if __name__ == "__main__":
